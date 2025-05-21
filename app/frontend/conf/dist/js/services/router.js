f49742737ea8185d0a499ced8fde0fbe33cf0e53
Alex <bokanchaalexandra@gmail.com>
2025-05-21 14:16:19 +0200

    prepa pour le merge

--- app/frontend/conf/dist/js/services/router.js
+++
@@ -1,5 +0,0 @@
-import { router } from '../main.js';
-export function navigateTo(url) {
-    window.history.pushState({}, '', url); // Met à jour l'URL dans la barre d'adresse sans recharger
-    router();
-}
