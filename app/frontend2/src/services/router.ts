80aea4cf57691044643d40f967e1afc26d06817e
aduvilla <aduvilla@sudent.42.fr>
2025-06-10 22:54:52 +0200

    suppression de src de frontend2

--- app/frontend2/src/services/router.ts
+++
@@ -1,6 +0,0 @@
-import { router } from '../main.js';
-
-export function navigateTo(url: string) {
-	window.history.pushState({}, '', url);	// Met à jour l'URL dans la barre d'adresse sans recharger
-	router();
-}
